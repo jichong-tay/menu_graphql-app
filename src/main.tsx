c4ca347bc9ba99e7dfc2319e60973af0173791b0
jichong <151018920+jichong-tay@users.noreply.github.com>
2025-01-19 04:18:48 +0800

    Setup Apollo Client

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import "./index.css";
+import App from "./App.tsx";
+import theme from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 
-createRoot(document.getElementById('root')!).render(
+const client = new ApolloClient({
+  uri: import.meta.env.VITE_BACKEND_URL || "http://localhost:3002/graphql",
+  cache: new InMemoryCache(),
+});
+
+createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </ApolloProvider>
+  </StrictMode>
+);
